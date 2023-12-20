94b41d12a7c161cca50d7b20ecec93b690163f6a
hayoung305 <hayoung305@naver.com>
2023-12-20 10:50:15 +0900

    로그인, 회원가입, 레이아웃

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,10 +2,21 @@
     <section id="wrap">
         <!-- 검색 엔진 최적화를 위해 콘텐츠롸 어울리는 제목을 h1태그에 텍스트로 넣음 -->
         <h1 class="blind">웹사이트 제목</h1>
-        <section>상단영역</section>
+        <Header />
         <section id="container">
             <router-view />
         </section>
-        <section>하단영역</section>
+        <Footer />
     </section>
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+export default {
+    components: {
+        Header,
+        Footer,
+    },
+};
+</script>
